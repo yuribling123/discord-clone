a6b0cb82fb27db565097d91af443d1a24fd80a42
milktea <yuribling123@gmail.com>
2024-08-07 00:15:44 -0700

    ✨ Search Channel

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,9 +1,13 @@
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { ServerHeader } from "./server-header";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { ServerSearch } from "./server-search";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { useEffect } from "react";
 
 interface ServerSidebarProps {
     serverId: string;
@@ -12,6 +16,23 @@ interface ServerSidebarProps {
 export const ServerSidebar = async ({
     serverId
 }: ServerSidebarProps) => {
+    const iconMap = {
+        [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+        [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+        [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />
+    };
+
+
+    const roleIconMap = {
+        [MemberRole.GUEST]: null,
+        [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />,
+        [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-indigo-500" />
+    };
+
+
+
+
+
     const profile = await currentProfile();
 
     // ensure the user is login
@@ -47,6 +68,9 @@ export const ServerSidebar = async ({
 
     const members = server?.members.filter((member) => member.profileId !== profile.id);
 
+
+
+
     if (!server) {
         return redirect("/");
     }
@@ -54,12 +78,61 @@ export const ServerSidebar = async ({
     const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
 
+
+    // an array of objects representing different categories of channels and members
+
     return (
         <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
-        <ServerHeader 
-          server={server}
-          role={role}
-        />
-      </div>
+            <ServerHeader
+                server={server}
+                role={role}
+            />
+
+            <ScrollArea className="flex-1 px-3">
+                <div className="mt-2"></div>
+                {/* search */}
+
+                <ServerSearch data={[{
+                    label: "Text Channels",
+                    type: "channel",
+                    data: textChannels?.map((channel) => ({
+                        id: channel.id,
+                        name: channel.name,
+                        icon: iconMap[channel.type],
+                    }))
+                },
+                {
+                    label: "Audio Channels",
+                    type: "channel",
+                    data: audioChannels?.map((channel) => ({
+                        id: channel.id,
+                        name: channel.name,
+                        icon: iconMap[channel.type],
+                    }))
+                },
+                {
+                    label: "Video Channels",
+                    type: "channel",
+                    data: videoChannels?.map((channel) => ({
+                        id: channel.id,
+                        name: channel.name,
+                        icon: iconMap[channel.type],
+                    }))
+                },
+                {
+                    label: "Members",
+                    type: "member",
+                    data: members?.map((member) => ({
+                        id: member.id,
+                        name: member.profile.name,
+                        icon: roleIconMap[member.role],
+                    }))
+                },
+
+
+                ]} />
+
+            </ScrollArea>
+        </div>
     );
 };
