5612dc71c4349613191ab9f6eca38b9c68e216ec
milktea <yuribling123@gmail.com>
2024-08-08 18:43:34 -0700

    🚧 Text Input Bar

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,9 @@
 import ChatHeader from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import { ChartBar } from "lucide-react";
 import { redirect } from "next/navigation";
 
 interface ChannelIdPageProps {
@@ -31,17 +33,25 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         },
     });
 
-    if (!member||!channel){
+    if (!member || !channel) {
         redirect("/")
     }
 
 
 
     return (
-        <div>
-           {/* chat header */}
-           <ChatHeader name={channel.name} serverId={channel.serverId} type={"channel"} ></ChatHeader>
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            {/* chat header */}
+            <ChatHeader name={channel.name} serverId={channel.serverId} type={"channel"} imageUrl={""} ></ChatHeader>
+            <div className="flex-1">Future Messages</div>
+            {/* chat input */}
+
+         
+                <ChatInput apiUrl={"/api/socket/messages"} query={{ channelId: channel.id, serverId: channel.serverId }} name={channel.name} type={"channel"}></ChatInput>
+    
+
         </div>
+
     );
 }
 
